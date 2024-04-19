3435aafd930019958502a4cb207696858247c133
r.tunga <ruitunga20@gmail.com>
2024-04-19 19:09:20 +0100

    19-04-2025

--- landpage/qr.py
+++ landpage/qr.py
@@ -1,6 +1,6 @@
 import qrcode
 
-data = 'https://cc.mirempet.ao'
+data = 'https://cc9.mirempet.ao'
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -11,4 +11,4 @@ qr.add_data(data)
 qr.make(fit=True)
 
 img = qr.make_image(fill_color="black", back_color="white")
-img.save('qr_code.png')
+img.save('cc9.png')
