6704fb4760fe7912a2b2e2d6b543e5a9678b3274
r.tunga <ruitunga20@gmail.com>
2024-05-19 22:57:35 +0100

    19-05-2024 22:55

--- mirempet/settings.py
+++ mirempet/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wx@wh0$f%x0#0&7ingi=(fntq!91x@o!^%oyty5ivh&30@vh(_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://cc9.mirempet.ao','*']
 
